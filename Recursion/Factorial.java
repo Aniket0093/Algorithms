c99ff351b7c97cf23bec955c694aeeaec3626684
Aniket Nawade <31523559+Aniket0093@users.noreply.github.com>
2020-01-28 22:47:25 -0500

    Recursion

---
+++ Recursion/Factorial.java
@@ -0,0 +1,13 @@
+package Recursion;
+
+public class Factorial {
+	
+	public int fact(int n){
+		
+		if ( n < 1 ) {
+			return 1;
+		} else {
+			return( n * fact( n - 1));
+		}
+	}
+}
---
+++ Recursion/Main.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Factorial
+		Factorial f = new Factorial();
+		
+		System.out.println(f.fact(4));
+		
+		//Fibonacci
+		Fibonacci fi = new Fibonacci();
+		
+		System.out.println(fi.fibo(5));
+
+	}
+
+}
